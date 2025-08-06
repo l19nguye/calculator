dd1941c603b41d93144d50ec7d46dae5c2a92317
Lam <mr.dinhlam@gmail.com>
2025-08-06 23:16:28 +0700

    change

--- lambda_function.py
+++ lambda_function.py
@@ -15,6 +15,6 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             'statusCode': 400,
-            'message': f'exception {str(e)} occurred'
+            'message': 'exception occurred'
         }
     
